040320d11736cfa67e15bf9d1738a83aca2425fd
Didier Verna <didier@didierverna.net>
2017-01-19 15:28:51 +0100

    Upgrade the license to LPPL v1.3.

--- fixme.el
+++ fixme.el
@@ -1,29 +1,22 @@
 ;;; fixme.el --- AUC-TeX style file for FiXme
 
-;; Copyright (C) 2000, 2002, 2004, 2006, 2009 Didier Verna
+;; Copyright (C) 2000, 2002, 2004, 2006, 2009, 2017 Didier Verna
 
 ;; Author: Didier Verna <didier@didierverna.net>
-;; Created: Tue Apr 18 14:49:29 2000
 ;; Keywords: tex abbrev data
-
 
 ;; This file is part of FiXme.
 
-;; FiXme may be distributed and/or modified under the
-;; conditions of the LaTeX Project Public License, either version 1.1
-;; of this license or (at your option) any later version.
-;; The latest version of this license is in
-;; http://www.latex-project.org/lppl.txt
-;; and version 1.1 or later is part of all distributions of LaTeX
-;; version 1999/06/01 or later.
+;; FiXme may be distributed and/or modified under the conditions of the LaTeX
+;; Project Public License, either version 1.3 of this license or (at your
+;; option) any later version.  The latest version of this license is in
+;; http://www.latex-project.org/lppl.txt and version 1.3 or later is part of
+;; all distributions of LaTeX version 2005/12/01 or later.
 
 ;; FiXme consists of all files listed in the file `README'.
 
 
 ;;; Commentary:
-
-;; Contents management by FCM version 0.1.
-
 
 
 ;;; Code:
